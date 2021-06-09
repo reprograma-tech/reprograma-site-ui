954f5cfd2cc41cd178ede683f74c61d5cb4baeff
Jo Correa <jo.correa@thoughtworks.com>
2021-06-09 12:09:41 -0300

    Create banner data file

---
+++ src/components/Banner/data.js
@@ -0,0 +1,52 @@
+export const homeBanner = {
+  backgroundImage: "/images/banner/home.png",
+  title: "{reprograma}",
+  subtitle:
+    "Reduzindo o gap de gênero no setor de tecnologia por meio da educação.",
+  buttons: [
+    {
+      label: "Participe",
+      alternativeText: "Ir para cursos",
+      to: "/#courses",
+      variant: 'white'
+    },
+    {
+      label: "Apoie",
+      alternativeText: "Ir para parcerias",
+      to: "/#partners"
+    },
+  ],
+};
+
+export const projectsBanner = {
+  backgroundImage: "/images/banner/projects.png",
+  title: "Outros projetos",
+  subtitle: "Além dos curso, procuramos trazer conteúdos que ajudem a comunidade de tecnologia. Confira algumas iniciativas."
+};
+
+export const teamBanner = {
+  backgroundImage: "/images/banner/team.png",
+  title: "Quem está ajudando a reprogramar o mundo"
+};
+
+export const partnersBanner = {
+  backgroundImage: "/images/banner/partners.png",
+  title: "Faça parte da transformação"
+};
+
+export const hiringBanner = {
+  backgroundImage: "/images/banner/hiring.jpg",
+  title: "Contrate uma aluna"
+};
+
+export const allTechBanner = {
+  backgroundImage: "/images/banner/allTech.png",
+  title: "Todas em Tech",
+  subtitle: "Inspiração, apoio e capacitação de mulheres para diminuir a lacuna de gênero no setor de tecnologia."
+};
+
+export const newsBanner = {
+  backgroundImage: "/images/banner/home.png", // TODO: Add image (Mariel)
+  title: "Nos vemos por aí",
+  subtitle: "A {reprograma} foi notícia em vários veículos do país e do mundo, além de ter sido reconhecida por instituições de prestígio internacional"
+};
