c7cc8663a2a4e8b31e6660fa57bda3fcd37a4704
Jo Correa <jo.correa@thoughtworks.com>
2021-06-10 10:31:39 -0300

    Create SectionOurTeam component

---
+++ src/components/Home/SectionOurTeam/index.jsx
@@ -0,0 +1,27 @@
+import styles from "./styles.module.scss";
+import { LinkButton } from "../../LinkButton";
+
+export function SectionOurTeam({ ourTeam }) {
+  return (
+    <section className={styles.sectionOurTeam}>
+      <img
+        alt={ourTeam.image.alternativeText}
+        src={ourTeam.image.url}
+      />
+      <div className={styles.teamBlock}>
+        <h3>{ourTeam.title}</h3>
+        <p>{ourTeam.text}</p>
+
+        {ourTeam.buttons.map((button, index) => (
+          <LinkButton
+            key={index}
+            label={button.label}
+            aria-label={button.alternativeText}
+            variant={button.variant ? button.variant : ""}
+            to={button.url}
+          />
+        ))}
+      </div>
+    </section>
+  );
+}
